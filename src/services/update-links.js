4bff701156591a538ccc72ca78ae9e856ee5cb64
Kevin Isom <kevin.isom@gmail.com>
2020-01-20 08:42:29 +1300

    get content for links that don't have anything in content

---
+++ src/services/update-links.js
@@ -0,0 +1,13 @@
+const { each } = require('@kev_nz/async-tools');
+const { db } = require('./db');
+const process = require('./process')
+module.exports = async () => {
+  const links = await db.links.find({
+    content: { $exists: false }
+  });
+
+  await each(links, async (link) => {
+    const read = await process(link.url, link)
+    await db.links.save({ ...link,...read })
+  });
+};
