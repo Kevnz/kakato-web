a8488cbcbaf445f722a1e8d5edbc85bebeb1830c
Kevin Isom <kevin.isom@gmail.com>
2020-01-23 16:17:19 +1300

    layouts

--- src/views/layout.jsx
+++ src/views/layout.jsx
@@ -1,64 +1,87 @@
 'use strict';
 const React = require('react');
-const { Header, NavigationBar } = require('mini.css-react');
+const { Button, NavBar, NavBarBrand, NavBarDivider, NavBarDropDown, NavBarEnd, NavBarItem, NavBarStart, NavBarMenu, Buttons } = require('@brightleaf/elements')
 const { If, Then, Else } = require('react-if');
-const { Component, Fragment } = React;
 
-class LayoutView extends Component {
-  render() {
-    //console.log('the laout props', this.props);
-    return (
-      <html>
-        <head>
-          <title>{this.props.title}</title>
-          <link href="/mini-dark.css" rel="stylesheet" />
-        </head>
-        <body>
-          <Header sticky>
-            <a href="#" logo>
-              Kakato
-            </a>
-            <a href="/" className="button">
-              Home
-            </a>
-            <If condition={this.props.isAuthenticated}>
-              <Then>
-                <span>
-                  <a href="/account" className="button">
-                    Account
-                  </a>
-                  <a href="/auth/twitter" className="button">
-                    Twitter
-                  </a>
-                  <a href="/auth/linkedin" className="button">
-                    LinkedIn
-                  </a>
-                </span>
-              </Then>
-              <Else>
-                {() => (
-                  // will only be evaluated if the condition fails.
-                  <Fragment>
-                    <a href="/login" className="button">
-                      Login
-                    </a>
-                    <a href="/account/new" className="button">
-                      {' '}
-                      Register{' '}
-                    </a>
-                  </Fragment>
-                )}
-              </Else>
-            </If>
-          </Header>
-          <div
-            id="content"
-            dangerouslySetInnerHTML={{ __html: this.props.children }}
+const LayoutView = (props) => {
+
+
+  return (
+    <html>
+      <head>
+        <title>{props.title}</title>
+        <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/litera/bulmaswatch.min.css" />
+
+      </head>
+      <body>
+        <NavBar isDark>
+          <NavBarBrand
+            src="/imgs/kakato-logo.png"
+            href="#"
+            target="navbarBasicExample"
+            width="112"
+            height="28"
           />
-        </body>
-      </html>
-    );
-  }
+          <NavBarMenu id="navbarBasicExample">
+            <NavBarStart>
+              <NavBarItem><a>Home</a></NavBarItem>
+              <NavBarItem><a>Documentation</a></NavBarItem>
+              <NavBarDropDown title="Examples">
+                <NavBarItem><a to="/containers">Container</a></NavBarItem>
+                <NavBarItem>
+                  <a to="/columns">Columns</a>
+                </NavBarItem>
+                <NavBarItem>
+                  <a to="/notifications">Notifications</a>
+                </NavBarItem>
+                <NavBarItem>
+                  <a to="/autocomplete">AutoComplete</a>
+                </NavBarItem>
+                <NavBarDivider />
+                <NavBarItem>
+                  <a>Report an issue</a>
+                </NavBarItem>
+              </NavBarDropDown>
+            </NavBarStart>
+
+            <NavBarEnd>
+              <NavBarItem>
+                <div>
+                  <If condition={props.isAuthenticated}>
+                    <Then>
+                      <Button isPrimary isAnchor href="/account">
+                        Account
+                      </Button>
+                    </Then>
+                    <Else>
+                      {() => (
+                        // will only be evaluated if the condition fails.
+                        <Buttons>
+                          <Button isPrimary isAnchor href="/account/new">
+                            <strong>Sign up</strong>
+                          </Button>
+                          <Button isLight isAnchor href="/login" >
+                            Log in
+                            </Button>
+                        </Buttons>
+                      )}
+                    </Else>
+                  </If>
+
+                </div>
+              </NavBarItem>
+            </NavBarEnd>
+          </NavBarMenu>
+        </NavBar>
+
+        <div
+          id="content"
+
+        >{props.children}</div>
+      </body>
+    </html>
+  );
+
 }
 
 module.exports = LayoutView;
