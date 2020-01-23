5aff2b9dbe3fcccce7ce42eab8139e37abe76720
Kevin Isom <kevin.isom@gmail.com>
2020-01-23 16:18:36 +1300

    change structure and add layout

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,27 +1,43 @@
 'use strict';
 const React = require('react');
-const { InputGroup } = require('mini.css-react');
-
+const { Section, Container, Title } = require('@brightleaf/elements')
+const Layout = require('./layout')
 class LoginView extends React.Component {
   render() {
     return (
-      <form method="POST">
-        <fieldset>
-          <legend> Login</legend>
+      <Layout>
+        <Section>
+          <Container>
+            <form method="POST" className="form">
+              <fieldset>
+                <legend><Title>Login</Title></legend>
+                <br />
 
-          <InputGroup>
-            <label> Email </label>
-            <input type="text" name="email" />
-          </InputGroup>
-          <InputGroup>
-            <label> Password </label>
-            <input type="password" name="password" />
-          </InputGroup>
-          <InputGroup>
-            <input type="submit" value="Login" />
-          </InputGroup>
-        </fieldset>
-      </form>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input name="email" className="input" type="text" placeholder="Email" />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control">
+                    <input name="password" className="input" type="password" placeholder="Password" />
+                  </div>
+                </div>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <input type="submit" className="button is-link is-radiusless" value="Login" />
+                  </div>
+                  <div className="control">
+                    <button className="button is-radiusless">Cancel</button>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </Container>
+        </Section>
+      </Layout>
     );
   }
 }
